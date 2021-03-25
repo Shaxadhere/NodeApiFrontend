e3cd0740741f4d296dd1e5019971702feccd71d6
Shaxadhere <Shaxad.here@gmail.com>
2021-03-25 09:43:20 +0500

    create post -p

---
+++ CreatePost.php
@@ -0,0 +1,14 @@
+<button id="btnBack" type="button" class="btn btn-secondary">Back</button>
+<form>
+    <div class="form-group">
+        <label for="Title">Title</label>
+        <input type="text" class="form-control" id="Title" placeholder="Enter Title">
+        <small id="TitleHelp" class="form-text text-muted">Please enter title of your post.</small>
+    </div>
+    <div class="form-group">
+        <label for="Body">Body</label>
+        <input type="text" class="form-control" id="Body" placeholder="Enter Body">
+        <small id="BodyHelp" class="form-text text-muted">Please enter body of your post.</small>
+    </div>
+    <button id="CreatePost" type="submit" class="btn btn-primary">Submit</button>
+</form>
--- index.php
+++ index.php
@@ -10,9 +10,11 @@
 </head>
 
 <body>
+
     <div class="container-fluid">
-        <div class="container">
-            <table class="table" style="margin-top:80px !important">
+        <div id="root" class="container" style="margin-top:80px !important">
+            <button id="btnGetCreateForm" type="button" class="btn btn-primary">Create Post</button>
+            <table class="table" style="margin-top:10px !important">
                 <thead>
                     <tr>
                         <th>SNo</th>
@@ -32,6 +34,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 <script>
+    //Get List
     $(document).ready(function() {
         $.ajax({
             type: "GET",
@@ -52,6 +55,11 @@
                 console.log("error: " + error)
             }
         })
+    })
+
+    //Show Add Form
+    $(document).on('click', '#btnGetCreateForm', function(){
+        
     })
 </script>
 
