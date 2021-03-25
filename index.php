c4bc3579b3a7b8361ff97e429079ffc28eead990
Shaxadhere <Shaxad.here@gmail.com>
2021-03-25 09:34:59 +0500

    list from api

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="container">
+            <table class="table" style="margin-top:80px !important">
+                <thead>
+                    <tr>
+                        <th>SNo</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                    </tr>
+                </thead>
+                <tbody id="table-body">
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:8080",
+            success: function(response) {
+                var stringfied = JSON.stringify(response)
+                var result = JSON.parse(stringfied);
+                var posts = result['posts'];
+                var Sno = 1;
+                posts.forEach(value => {
+                    var RowTemplate = "<tr><td>"+Sno+"</td><td>"+value['title']+"</td><td>"+value['body']+"</td></tr>";
+                    $('#table-body').append(RowTemplate)
+                    Sno++;
+                });
+                
+            },
+            error: function(error) {
+                console.log("error: " + error)
+            }
+        })
+    })
+</script>
+
+</html>
